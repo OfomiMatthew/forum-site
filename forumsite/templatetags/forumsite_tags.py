9d812bba5905f68aaf5515b2ecb070f5e4d1c2d2
OfomiMatthew <ofomimatthew7@gmail.com>
2024-05-06 23:34:36 +0100

    implemented template filter and sitemap

--- forumsite/templatetags/forumsite_tags.py
+++ forumsite/templatetags/forumsite_tags.py
@@ -1,5 +1,7 @@
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 from . .models import Post 
 
 register = template.Library()
@@ -18,3 +20,7 @@ def show_latest_posts(count=3):
 @register.simple_tag
 def most_commented_posts(count=3):
     return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
